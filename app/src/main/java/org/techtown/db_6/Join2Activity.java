18720cdeccffb729ce89f564f87845a32ecc7204
나유경 <yookyoung0503@gmail.com>
2021-09-07 00:38:18 +0900

    이용약관기능 개미찡

--- app/src/main/java/org/techtown/db_6/Join2Activity.java
+++ app/src/main/java/org/techtown/db_6/Join2Activity.java
@@ -2,10 +2,14 @@ package org.techtown.db_6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class Join2Activity extends AppCompatActivity {
     private Button startbutton1;
@@ -14,15 +18,86 @@ public class Join2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join2);
 
-
+        CheckBox checkBox=findViewById(R.id.checkBox2);
+        CheckBox checkBox2=findViewById(R.id.checkBox3);
+        CheckBox checkBox3=findViewById(R.id.checkBox4);
+        CheckBox checkBox4=findViewById(R.id.checkBox5);
         startbutton1 = (Button) findViewById(R.id.startbutton1);
+
+
+        checkBox.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(checkBox.isChecked()){
+                    checkBox2.setChecked(true);
+                    checkBox3.setChecked(true);
+                    checkBox4.setChecked(true);
+                }
+                else{
+                    checkBox2.setChecked(false);
+                    checkBox3.setChecked(false);
+                    checkBox4.setChecked(false);
+                }
+
+            }
+        });
+        checkBox2.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+
+                if(checkBox.isChecked()){
+                    checkBox.setChecked(false);
+                }
+                else if(checkBox2.isChecked()&&checkBox3.isChecked()&&checkBox4.isChecked()){
+                    checkBox.setChecked(true);
+                }
+
+            }
+        });
+
+        checkBox3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(checkBox.isChecked()){
+                    checkBox.setChecked(false);
+
+                }
+                else if(checkBox2.isChecked()&&checkBox3.isChecked()&&checkBox4.isChecked()){
+                    checkBox.setChecked(true);
+                }
+
+            }
+        });
+
+        checkBox4.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if(checkBox.isChecked()){
+                    checkBox.setChecked(false);
+                }
+                else if(checkBox2.isChecked()&&checkBox3.isChecked()&&checkBox4.isChecked()){
+                    checkBox.setChecked(true);
+                }
+            }
+
+        });
 
         startbutton1.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), JoinActivity.class);
-                startActivity(i);
+                if(checkBox.isChecked()&&checkBox2.isChecked()&&checkBox3.isChecked()&&checkBox4.isChecked()){
+                    Intent i = new Intent(getApplicationContext(), JoinActivity.class);
+                    startActivity(i);
+                }
+                else if(checkBox2.isChecked()&&checkBox3.isChecked()){
+                    Intent i = new Intent(getApplicationContext(), JoinActivity.class);
+                    startActivity(i);
+                }
+                else{
+
+                    Toast.makeText(getApplicationContext(),"필수약관동의에 체크해주세요",Toast.LENGTH_LONG).show();
+
+
+                }
+
             }
         });
     }
