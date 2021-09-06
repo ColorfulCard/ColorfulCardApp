dd400341f6c292c7698c18777c9942300a4d55e2
오은아 <erropp89@naver.com>
2021-09-07 03:17:11 +0900

    commit_click

---
+++ app/src/main/java/org/techtown/db_6/MapdataIitem.java
@@ -0,0 +1,14 @@
+package org.techtown.db_6;
+
+public class MapdataIitem {
+
+    private String textView;
+
+    public MapdataIitem(String textView) {
+        this.textView = textView;
+    }
+
+    public String getTextView() {
+        return textView;
+    }
+}
