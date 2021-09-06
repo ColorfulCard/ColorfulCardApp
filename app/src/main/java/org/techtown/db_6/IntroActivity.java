ae158426f44e8747d3e5efe0a2d683e156afe778
나유경 <yookyoung0503@gmail.com>
2021-09-06 14:00:13 +0900

    intro and login push

---
+++ app/src/main/java/org/techtown/db_6/IntroActivity.java
@@ -0,0 +1,34 @@
+package org.techtown.db_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+//대기화면 Activity
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        //핸들러 선언
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent (getApplicationContext(), LoginActivity.class); //Intro화면에서 Login 화면으로 넘어가는 intent
+                startActivity(intent); //intent 시작
+                finish();
+            }
+        },3000);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
