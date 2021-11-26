def54f4f1347536aefec93981527a9696dd9e1ee
eunalove <88129325+eunalove@users.noreply.github.com>
2021-11-26 21:40:06 +0900

    게시판UI

---
+++ app/src/main/java/org/techtown/db_6/WritingPosting.java
@@ -0,0 +1,14 @@
+package org.techtown.db_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WritingPosting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writing_posting);
+    }
+}
