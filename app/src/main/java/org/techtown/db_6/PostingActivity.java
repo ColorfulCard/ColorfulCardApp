6065f0347e93ce9901e6b1972e3e0dea92749194
codingGoover <dpswpfgkxm30@naver.com>
2021-11-29 19:40:27 +0900

    postingActivity- 댓글 대댓글 불러오기

--- app/src/main/java/org/techtown/db_6/PostingActivity.java
+++ app/src/main/java/org/techtown/db_6/PostingActivity.java
@@ -2,11 +2,14 @@ package org.techtown.db_6;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +18,13 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class PostingActivity extends AppCompatActivity {
 
     BottomNavigationView navigationView;
@@ -22,7 +32,12 @@ public class PostingActivity extends AppCompatActivity {
     Posting posting;
     TextView pid,pcontent,pdate,hcnt,ccnt,vcnt;
     Button deleteBt;
-    private String userID= BoardActivity.userID;
+    static public String userID= BoardActivity.userID;
+    private ArrayList<Comment> comments = new ArrayList<>();
+    private ArrayList<Ccomment> ccomments = new ArrayList<>();
+    private MainHandler handler;
+    private ArrayList<DataItem.CommentData> dataList = new ArrayList<>();;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +57,7 @@ public class PostingActivity extends AppCompatActivity {
         vcnt=findViewById(R.id.vcnt);
         deleteBt= findViewById(R.id.deleteBt);
 
+        recyclerView= findViewById(R.id.recyclerView4);
 
         pid.setText(posting.getPid());
         pcontent.setText(posting.getPcontent());
@@ -56,8 +72,120 @@ public class PostingActivity extends AppCompatActivity {
             //본인 글이면 삭제하기 버튼 나타남
         }
 
+        handler = new MainHandler();
+
+        //들어올 때 해당포스팅에 달린 댓글 대댓글있으면 들고와야한다.
+        if(posting.getCcnt()>0){
+            GetCmentsThread getCmentThread= new GetCmentsThread(posting.getPno());
+            getCmentThread.start();
+        }
+
 
     }// onCreate()..
+
+    class GetCmentsThread extends Thread{
+
+        int pno;
+        public GetCmentsThread(int pno){
+            this.pno=pno;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+
+            Message message = handler.obtainMessage();
+
+            Server server = new Server();
+            RetrofitService service = server.getRetrofitService();
+            Call<List<Comment>> call = service.getComment(pno);
+
+            call.enqueue(new Callback<List<Comment>>() {
+                @Override
+                public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+
+                    if(response.isSuccessful()){
+
+                        comments= (ArrayList<Comment>)response.body();
+                        message.what= StateSet.BoardMsg.MSG_SUCCESS_GETCMENTS;
+                        handler.sendMessage(message);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<Comment>> call, Throwable t) {
+                    message.what= StateSet.BoardMsg.MSG_FAIL;
+                    handler.sendMessage(message);
+
+                }
+            });
+
+        }
+    }
+
+    void getCcmentsOfCment (int pno, int cno, int i){
+
+        Server server= new Server();
+        RetrofitService service= server.getRetrofitService();
+        Call<List<Ccomment>> call= service.getCcomment(pno,cno);
+
+        call.enqueue(new Callback<List<Ccomment>>() {
+            @Override
+            public void onResponse(Call<List<Ccomment>> call, Response<List<Ccomment>> response) {
+
+                List<Ccomment> results= response.body();
+                if(response.isSuccessful())
+                    if(i==0) {
+                        ccomments = (ArrayList<Ccomment>) results;
+                    }
+                    else
+                    {
+                        for(Ccomment ccment: results) {
+                            ccomments.add(ccment);
+                        }
+                    }
+            }
+
+            @Override
+            public void onFailure(Call<List<Ccomment>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),"네트워크를 확인하세요.",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    class CheckHeartThread extends Thread{
+
+        @Override
+        public void run() {
+            super.run();
+        }
+    }
+
+    class InsertCmentThread extends Thread{
+
+        @Override
+        public void run() {
+            super.run();
+        }
+    }
+
+    class DeletePostingOfBoard extends Thread{
+
+        @Override
+        public void run() {
+            super.run();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    private void updatePostingOfBoard(){
+
+
+    }
 
     class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
         @Override
@@ -89,37 +217,43 @@ public class PostingActivity extends AppCompatActivity {
     }// ItemSelectedListener class..
 
 
-    class GetCmentThread extends Thread{
-
-
-        @Override
-        public void run() {
-            super.run();
-        }
-    }
-
-    class GetCcmmentsThread extends Thread{
+    class MainHandler extends Handler{
 
         @Override
-        public void run() {
-            super.run();
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+
+            switch(msg.what){
+                case StateSet.BoardMsg.MSG_SUCCESS_GETCMENTS:
+
+                    if(comments.isEmpty()){
+                        break;
+                    }
+                    int i=0;
+
+                    for(Comment cment:comments){
+
+                        dataList.add( new DataItem.CommentData(cment,StateSet.ViewType.comment));
+                        Log.d("tag","dataList에 cno:"+cment.getCno()+"추가함");
+                        if(cment.getCccnt()>0){
+                            int prevSize= ccomments.size();
+                            getCcmentsOfCment(posting.getPno(),cment.getCno(),i++);
+
+                            for(int j=prevSize ;j<ccomments.size();j++){
+                                Ccomment ccment= ccomments.get(j);
+                                dataList.add( new DataItem.CommentData(ccment,StateSet.ViewType.ccomment));
+                                Log.d("tag","dataList에 cno:"+ccment.getCno()+"의 ccno:"+ccment.getCcno()+"추가함");
+                            }
+                        }
+                    }
+
+                    recyclerView.setAdapter(new CmentListAdapter(dataList));
+                    break;
+
+                case StateSet.BoardMsg.MSG_FAIL:
+                    Toast.makeText(getApplicationContext(),"네트워크를 확인하세요.",Toast.LENGTH_SHORT).show();
+                    break;
+            }
         }
     }
-
-    class CheckHeartThread extends Thread{
-
-        @Override
-        public void run() {
-            super.run();
-        }
-    }
-
-    class InsertCmentThread extends Thread{
-
-        @Override
-        public void run() {
-            super.run();
-        }
-    }
-
 }// MainActivity class..
